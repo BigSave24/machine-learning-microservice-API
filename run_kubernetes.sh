2ee9644fed1674df4be824906f470024006001a5
Kedmond24 <Kedmond24@live.com>
2021-04-15 01:30:16 -0400

    ADD commands to deploy container to k8 cluster

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,19 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath=kedmond24/mlprediction
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+docker login -u kedmond24
+kubectl run mlprediction \
+    --image=$dockerpath:v1.0 \
+    --port=80
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward mlprediction 8000:80

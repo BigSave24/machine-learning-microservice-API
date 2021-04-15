759d30c191491904eb5b05af93e1a169258a915a
Kedmond24 <Kedmond24@live.com>
2021-04-15 01:31:36 -0400

    ADD commands to upload docker image to remote repository

--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,14 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath=kedmond24/mlprediction
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login -u kedmond24
+docker tag 1cab423ae4cb $dockerpath:v1.0
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath:v1.0
